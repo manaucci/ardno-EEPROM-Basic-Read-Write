a09071163d6ab0bfa7c390ce11f5b4183fac54ee
manaucci <manaucci@users.noreply.github.com>
2015-12-28 00:18:48 -0800

    Create control-LED-via-Serial.ino

---
+++ control-LED-via-Serial.ino
@@ -0,0 +1,20 @@
+/*
+
+Write a program that allows the user to control the LED connected to pin 13 of the Arduino.
+When the program is started, the LED should be off. The user should open the serial monitor to communicate with the Arduino.
+If the user sends the character '1' through the serial monitor then the LED should turn on. 
+If the user sends the character '0' through the serial monitor then the LED should turn off.
+
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
